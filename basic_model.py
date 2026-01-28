f0ae53cd792a468d76cffbb14001ae92cdffd974
Sandro <weicksa@web.de>
2026-01-28 15:22:28 +0100

    minor changes

--- basic_model.py
+++ basic_model.py
@@ -3,9 +3,8 @@ from load_data import load_data
 
 corpus_comp = load_data("corpus_comp")
 
-cv_classifier_subreddit = Classifier(
-    obj_type="utterance", pred_feats=["politeness_strategies"],
-    labeller=lambda u: u.meta["label"] == 1)
+cv_classifier_subreddit = Classifier(obj_type="utterance", pred_feats=["politeness_strategies"],
+                                     labeller=lambda u: u.meta["label"] == 1)
 
 cv_results = cv_classifier_subreddit.evaluate_with_cv(corpus_comp)
 

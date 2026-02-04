ca55666f7204237a675e70953ef8df8e4c91b998
Sandro <weicksa@web.de>
2026-02-04 09:48:30 +0100

    added ELFEN feature extraction - Warning - untested

--- extract_features.py
+++ extract_features.py
@@ -1,36 +1,59 @@
-from convokit import Corpus, PolitenessStrategies
+from convokit import Corpus, PolitenessStrategies, 
+BoWTransformer
+from elfen.extractor import Extractor
+
+import Sys
+import polars as pl
+
 from load_data import load_data
 
-# since there is way less data in the r/bookscirclejerk corpus
-# and because of computational limits I trimmed the r/Books
-# Corpus to be of ruffly equal size.
+# one function for convokit features
+def get_convo_features(corpus):
+    # get PolitenessStrategies from convokit
+    pol_strategies = PolitenessStrategies()
+    # get BoWTransformer from convokit
+    bow_trans = BoWTransformer()
+    # extract features
+    corpus = pol_strategies.transform(corpus, markers=True)
+    corpus = bow_trans.transform(corpus)
+    return(corpus)
 
-corpus_books_small = load_data("corpus_books_parsed")
-corpus_circle = load_data("corpus_circle_parsed")
+# one function for *all* ELFEN features
+# input for elfen feature extractor expects a pandas dataframe
+def get_elfen_features(corpus):
+    db_corp = pl.from_pandas(corpus)
+    extractor = Extractor(data = db_corp)
+    # extract *all* features that ELFEN provides
+    # WARNING: this requires additional lexicons to be installed manually
+    feats = extractor.extract_features()
 
-# for comparison:
-# r/books - subset
-# number of Utterancees 20000
-
-# r/bookscirclejerk - full (without empty entries)
-# number of Utterances 15850
-
-
-# get PolitenessStrategies from convokit
-pol_strategies = PolitenessStrategies()
-
-corpus_books_small = pol_strategies.transform(corpus_books_small, markers=True)
-corpus_circle = pol_strategies.transform(corpus_circle)
-
-# INSERT further feature extraction here:
+    # returns a polars database
+    return(extractor)
 
 
-corpus_books_small.dump("corpus_books_feat")
-corpus_circle.dump("corpus_circle_feat")
+if __name__ == "__main__":
+    # get filename from terminal argument
+    filename = Sys.argv[0]
+    # default filenames of parsed data are:
+    # - corpus_books_parsed
+    # - corpus_circle_parsed
+    data = load_data(filename)
+    print(f"\n {40*-}\nExtracting Convokit features ...")
+    conv_feat = get_convo_features(data)
+    print(f"\n {40*-}\n- Done Extracting Convokit features -")
+    # convert Convokit corpus to Pandas Dataframe
+    print(f"\n {40*-}\n- Saving Corpus with only Convokit features ...")
+    conv_savename = f"convo_{filename}"
+    # default location of dump() is {user}/.convokit/saved-corpora
+    conv_feat.dump(conv_savename)
+    print(f"\n {40*-}\n- Done saving Convokit features -")
+    data_panda_df = conv_feat.get_utterances_dataframe()
+    print(f"\n {40*-}\nExtracting ELFEN features ...")
+    elfen_feat = get_elfen_features(data_panda_df)
+    print(f"\n {40*-}\n- Done Extracting ELFEN features -")
+    print(f"\n {40*-}\n- Saving Corpus as Polars Database ...")
 
-# combine corpora
-corpus_comp = corpus_books_small.merge(corpus_circle, corpus_books_small)
+    savename = f"feats_{filename}.json"
+    elfen_feat.data.write_ndjson(savename)
+    print(f"\n {40*-}\n- Done saving -")
 
-# save combined corpus
-# default location is: .convokit/saved-corpora
-corpus_comp.dump("corpus_comp")
